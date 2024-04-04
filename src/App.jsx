288790afeedfe85d6a0244dc1ab68a7b2dd25390
fd-Martin <fdmosharof@gmail.com>
2024-04-04 09:44:16 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-9xl  font-bold ">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
